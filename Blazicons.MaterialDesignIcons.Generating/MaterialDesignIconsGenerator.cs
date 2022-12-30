1d03f4adf5c7e54f3adedeac27f39e595f4f59d1
Kyle Herzog <holler@kyleherzog.com>
2022-12-30 17:05:11 -0600

    Initial commit

---
+++ Blazicons.MaterialDesignIcons.Generating/MaterialDesignIconsGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Blazicons.Generating;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.Threading;
+
+namespace Blazicons.MaterialDesignIcons.Generating;
+
+[Generator]
+internal class MaterialDesignIconsGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        using var taskContext = new JoinableTaskContext();
+        var taskFactory = new JoinableTaskFactory(taskContext);
+        var downloader = new RepoDownloader(new Uri(Properties.Resources.DownloadAddress));
+        taskFactory.Run(
+            async () =>
+            {
+                await downloader.Download(@"^svg\/.*.svg$").ConfigureAwait(true);
+            });
+
+        var svgFolder = Path.Combine(downloader.ExtractedFolder, $"{downloader.RepoName}-{downloader.BranchName}", "svg");
+        context.WriteIconsClass("MdiIcon", svgFolder);
+
+        downloader.CleanUp();
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        // required by ISourceGenerator
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Blazicons.MaterialDesignIcons
+Provides the Material Design Icons icon library packaged as [Blazicons](https://github.com/kyleherzog/Blazicons), SVG icon components for Blazor.
+
+Check out the [Demo Site](http://blazicons.com).
+
+[![Build Status](https://dev.azure.com/kyleherzog/Blazicons/_apis/build/status/Blazicons.MaterialDesignIcons?branchName=main)](https://dev.azure.com/kyleherzog/Blazicons/_build/latest?definitionId=22&branchName=main)
+
+## Getting Started
+To get started using the Material Design Icons Blazicons, just install the Blazicons.MaterialDesignIcons NuGet package.
+
+Next add the Blazicons reference to the `_Imports.razor` file in the Blazor project.
+
+```csharp
+@using Blazicons
+```
+
+Finally, add the Blazicon components to your Blazor pages/components.
+```html
+<Blazicon Svg="MdiIcon.AccountGroup"></Blazicon>
+```
+
+## Parameters & Styling
+See the [Blazicons](https://github.com/kyleherzog/Blazicons) documentation for details on parameters and styling.
